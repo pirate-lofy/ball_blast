f300239100c454701eb2bf289d1abefbd229d405
Nour Helal <pirate.lofy@gmail.com>
2020-10-29 21:34:48 +0200

    Update train.py

--- train.py
+++ train.py
@@ -5,5 +5,5 @@ from env import Env
 
 e=Env(800,600)
 model = DQN(CnnPolicy, e, verbose=2)
-model.learn(total_timesteps=50000)
+model.learn(total_timesteps=100000)
 model.save("ball_blast_dqn_50000")
